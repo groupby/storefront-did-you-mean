a344e7ad25303d2832a325a7788320c71bfed9f5
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-24 22:21:39 -0400

    feat(aliasing v3): improve performance of aliasing and re-rendering (#23)

--- src/did-you-mean/index.ts
+++ src/did-you-mean/index.ts
@@ -1,29 +1,31 @@
-import { alias, configurable, origin, tag, utils, Events, Selectors, Store, Tag } from '@storefront/core';
+import { configurable, origin, provide, tag, utils, Events, Selectors, Store, Tag } from '@storefront/core';
 
 @configurable
-@alias('didYouMean')
+@provide('didYouMean')
 @origin('dym')
 @tag('gb-did-you-mean', require('./index.html'))
 class DidYouMean {
-
   state: DidYouMean.State = {
-    didYouMeans: []
+    didYouMeans: [],
   };
 
   init() {
-    this.updateDidYouMeans(this.select(Selectors.didYouMean));
     this.subscribe(Events.DID_YOU_MEANS_UPDATED, this.updateDidYouMeans);
   }
 
+  onBeforeMount() {
+    this.updateDidYouMeans(this.select(Selectors.didYouMean));
+  }
+
   updateDidYouMeans = (didYouMeans: string[]) =>
-    !(didYouMeans.length === 0 && this.state.didYouMeans.length === 0)
-    && this.set({ didYouMeans: utils.mapToSearchActions(didYouMeans, <any>this.actions) })
+    !(didYouMeans.length === 0 && this.state.didYouMeans.length === 0) &&
+    this.set({ didYouMeans: utils.mapToSearchActions(didYouMeans, <any>this.actions) });
 }
 
-interface DidYouMean extends Tag<any, DidYouMean.State> { }
+interface DidYouMean extends Tag<any, DidYouMean.State> {}
 namespace DidYouMean {
   export interface State {
-    didYouMeans: Array<{ value: string, onClick: () => void }>;
+    didYouMeans: Array<{ value: string; onClick: () => void }>;
   }
 }
 
